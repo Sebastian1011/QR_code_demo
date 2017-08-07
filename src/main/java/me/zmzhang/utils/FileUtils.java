07e6050b55b5e5c06655bc1e26e3bc4361951a7b
zmzhang <ziming.zhang@yitu-inc.com>
2017-08-07 11:25:41 +0800

    [website] version 1

---
+++ src/main/java/me/zmzhang/utils/FileUtils.java
@@ -0,0 +1,27 @@
+package me.zmzhang.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by zmzhang2 on 8/5/17.
+ */
+public class FileUtils {
+    public String getFile(String fileName) {
+        StringBuilder result = new StringBuilder("");
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
