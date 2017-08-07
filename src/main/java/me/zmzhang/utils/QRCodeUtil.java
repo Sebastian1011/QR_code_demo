fd173cbab87bc2d593fd62fe58e1825f0626a179
zmzhang <ziming.zhang@yitu-inc.com>
2017-08-07 19:28:47 +0800

    [website] multiple qr code in one image

--- src/main/java/me/zmzhang/utils/QRCodeUtil.java
+++ src/main/java/me/zmzhang/utils/QRCodeUtil.java
@@ -3,20 +3,22 @@ package me.zmzhang.utils;
 /**
  * Created by zmzhang2 on 8/5/17.
  */
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.multi.qrcode.QRCodeMultiReader;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 /**
  * 二维码工具类
@@ -27,12 +29,17 @@ public class QRCodeUtil {
     private static final String format = "png";// 默认二维码文件格式
     private int argb = 0xff000000; // 二维码颜色（ARGB）(默认值：0xff000000)
     private static final Map<EncodeHintType, Object> hints = new HashMap();// 二维码参数
+    private static final Map<DecodeHintType, Object> decodeHints = new HashMap();
 
     static {
         hints.put(EncodeHintType.CHARACTER_SET, "utf-8");// 字符编码
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);// 容错等级 L、M、Q、H 其中 L 为最低, H 为最高
         hints.put(EncodeHintType.MARGIN, 2);// 二维码与图片边距
         hints.put(EncodeHintType.QR_VERSION, 40); //二维码数据量
+    }
+    static {
+        decodeHints.put(DecodeHintType.CHARACTER_SET, "utf-8");
+        decodeHints.put(DecodeHintType.TRY_HARDER, true);
     }
     /**
      * 返回一个 BufferedImage 对象
@@ -68,11 +75,35 @@ public class QRCodeUtil {
         MatrixToImageWriter.writeToPath(bitMatrix, format, new File(path).toPath());
     }
 
-    public static void main(String[] args) throws WriterException, IOException {
-        String filePath = "QRCode.png";
-        FileUtils fileUtils = new FileUtils();
-        String qrCodeData = FileUtils.getFileContent(new File("test.csv"));
-        createQRCode(qrCodeData, filePath, 300, 300);
+    public static String decoder(String filename) throws NotFoundException, IOException {
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filename)))));
+        Result result = new MultiFormatReader().decode(binaryBitmap, decodeHints);
+        return result.getText();
+    }
+    public static String multiDecoder(String filename) throws IOException, FormatException, ChecksumException, NotFoundException {
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filename)))));
+        Result[] result = new QRCodeMultiReader().decodeMultiple(binaryBitmap, decodeHints);
+        String data = "";
+        for (Result item: result){
+            data += item.getText();
+        }
+        return data;
+    }
+
+
+    public static void main(String[] args) {
+//        String filePath = "QRCode.png";
+//        String qrCodeData = FileUtils.getFileContent(new File("test.csv"));
+//        createQRCode(qrCodeData, filePath, 300, 300);
+//        String filePath = "single.png";
+        try{
+            String filePath = "multi.png";
+            String result = multiDecoder(filePath);
+            System.out.print(result);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
         System.out.println("QR Code image created successfully!");
 
